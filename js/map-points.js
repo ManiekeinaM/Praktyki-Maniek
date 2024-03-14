d2d5d52d45375fa2f41f5aa95b10e62b0652fe88
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-03-14 21:58:25 +0100

    More points, connecting points

--- js/map-points.js
+++ js/map-points.js
@@ -4,15 +4,14 @@ const imgPaths = {
         // List of all dot coordinates placed around the map. Scaled to the image, then converted back into pixels.
         // Each dot needs a unique name
         dots: {
-            "dot1": {x: 0.17900, y: 0.38048}, // sala 2
-            "dot2": {x: 0.25200, y: 0.44272}, // sala 3
-            "dot3": {x: 0.25100, y: 0.74116}, // dyrektor
-            "dot4": {x: 0.24900, y: 0.88826}, // sala 10
-            "dot5": {x: 0.10200, y: 0.88119}, // sala 13
-            "dot6": {x: 0.10300, y: 0.65912}, // sekretariat uczniowski
-            "dot7": {x: 0.39300, y: 0.49646}, // sala 21
-            "dot8": {x: 0.53700, y: 0.49505}, // sala 24
-            "dot9": {x: 0.23700, y: 0.57709}, // maniek
+            "dot2": {x: 0.18500, y: 0.38048}, // sala 2
+            "dot3": {x: 0.25200, y: 0.46108}, // sala 3
+            "dot4": {x: 0.25100, y: 0.74882}, // dyrektor
+            "dot5": {x: 0.24900, y: 0.87972}, // sala 10
+            "dot6": {x: 0.10200, y: 0.87972}, // sala 13
+            "dot7": {x: 0.10300, y: 0.65912}, // sekretariat uczniowski
+            "dot8": {x: 0.39300, y: 0.49646}, // sala 21
+            "dot9": {x: 0.53700, y: 0.49505}, // sala 24
             "dot10": {x: 0.76000, y: 0.56011}, // sala 46
             "dot11": {x: 0.83700, y: 0.63366}, // sala 45
             "dot12": {x: 0.89100, y: 0.54173}, // kibel meski
@@ -24,6 +23,30 @@ const imgPaths = {
             "dot18": {x: 0.91000, y: 0.48091}, // schody b gora
             "dot19": {x: 0.90900, y: 0.43706}, // schody b dol
             "dot20": {x: 0.09200, y: 0.52900}, // schody a gora
+            "dot21": {x: 0.62083, y: 0.55896}, // mirys SALA
+
+            // W dots: walk dots
+            // You can not go back in this pathfinding system, so the connections are only one way
+            "Wdot0": {x: 0.23700, y: 0.57709, connections: ['Wdot1']}, // maniek
+            "Wdot1": {x: 0.23667, y: 0.54481, connections: ['Wdot2', 'Wdot7']}, // maniek up
+            "Wdot2": {x: 0.18500, y: 0.54481, connections: ['Wdot3', 'Wdot4', 'dot20']}, // leftA
+            "Wdot3": {x: 0.18500, y: 0.46108, connections: ['dot2', 'dot3']}, // up doors A
+            "Wdot4": {x: 0.18500, y: 0.65920, connections: ['dot7', 'Wdot5']}, // sekretariatSTAND
+            "Wdot5": {x: 0.18500, y: 0.74882, connections: ['dot4', 'Wdot6']}, // dyrektorSTAND
+            "Wdot6": {x: 0.18500, y: 0.87972, connections: ['dot6', 'dot5']}, // down doors A
+            "Wdot7": {x: 0.32083, y: 0.54481, connections: ['Wdot8']}, // maniek right turn
+            "Wdot8": {x: 0.32083, y: 0.43868, connections: ['Wdot9']}, // lacznik a up
+            "Wdot9": {x: 0.45667, y: 0.43868, connections: ['Wdot10', 'Wdot11']}, // lacznik middle
+            "Wdot10": {x: 0.45667, y: 0.49410, connections: ['dot8', 'dot9']}, // lacznik inbetween doors
+            "Wdot11": {x: 0.62083, y: 0.43868, connections: ['Wdot12']}, // lacznik b up
+            "Wdot12": {x: 0.62083, y: 0.47524, connections: ['dot21', 'Wdot13']}, // mirys STAND
+            "Wdot13": {x: 0.83333, y: 0.47524, connections: ['Wdot14', 'dot18', 'dot19', 'Wdot15']}, // middle B
+            "Wdot14": {x: 0.83333, y: 0.54307, connections: ['dot10', 'dot11', 'dot12']}, // down doors B
+            "Wdot15": {x: 0.83333, y: 0.36792, connections: ['dot13', 'Wdot16']}, // psychologkibel STAND CORRECT
+            "Wdot16": {x: 0.83333, y: 0.29363, connections: ['dot17', 'Wdot17']}, // sala 41 STAND
+            "Wdot17": {x: 0.83333, y: 0.21226, connections: ['dot14', 'Wdot18']}, // sala 32 STAND
+            "Wdot18": {x: 0.83333, y: 0.16038, connections: ['dot16', 'Wdot19']}, // sala 40 STAND
+            "Wdot19": {x: 0.83333, y: 0.11203, connections: ['dot15']}, // upmost B door STAND
         }
 
     },
@@ -63,12 +86,6 @@ const imgPaths = {
     },
 }
 
-// List of connections for each dot. There will be *many*.
-const connections = {
-
-}
-
-
 function percentToPx(percentages, relativeElement) {
     const bounds = relativeElement.getBoundingClientRect();
     return {
@@ -93,6 +110,8 @@ imgContainers.forEach(container => {
 
     container.style.width = `${img.width}px`;
     container.style.height = `${img.height}px`;
+    canvas.width = img.width;
+    canvas.height = img.height;
 })
 
 
@@ -101,22 +120,47 @@ imgContainers.forEach(container => {
 
 for (const [floorName, floorItems] of Object.entries(imgPaths)) {
     let floorContainer = document.querySelector(`div.${floorName}`);
+    if (!floorContainer) continue;
+
     let img = floorContainer.querySelector("img");
     let canvas = floorContainer.querySelector("canvas");
 
-    canvas.width = img.width;
-    canvas.height = img.height;
+    // canvas.width = img.width;
+    // canvas.height = img.height;
 
     let ctx = canvas.getContext("2d");
-    for (const [dot, coords] of Object.entries(floorItems.dots)) {
-        let coordsPx = percentToPx(coords, img);
+    for (const [dot, values] of Object.entries(floorItems.dots)) {
+        // 'values' contains: {x, y, connections}
+        let coordsPx = percentToPx(values, img);
 
         let draw = function() {
+            // Draw the dot
             ctx.fillStyle = "red";
             ctx.beginPath();
             ctx.arc(coordsPx.x, coordsPx.y, 5, 0, 2 * Math.PI);
             ctx.fill();
-        }
+
+            // Connect all connection dots
+            if (values.connections && values.connections.length > 0) {
+                ctx.strokeStyle = "red";
+                ctx.lineWidth = 3;
+
+                values.connections.forEach(connectionName => {
+                    let connectionDot = floorItems.dots[connectionName];
+                    let connectionCoordsPx = percentToPx(connectionDot, img);
+
+                    ctx.beginPath();
+                    ctx.moveTo(coordsPx.x, coordsPx.y);
+                    ctx.lineTo(connectionCoordsPx.x, connectionCoordsPx.y);
+                    ctx.stroke();
+                });
+            }
+
+            // Name the dot
+            ctx.fillStyle = "black";
+            ctx.font = "12px Arial";
+            ctx.fillText(dot, coordsPx.x, coordsPx.y - 10);
+        };
 
         if (img.complete) {
             draw();
@@ -166,12 +210,8 @@ document.addEventListener("keydown", e => {
 
     let x = e.key;
     
-    if (x.length == 2 || x.length == 3) return;
-    if (
-        (/[a-zA-Z0-9 ]/).test(x)
-        || e.key === "Enter" 
-        || e.key === "Backspace"
-        ) {
+    if (x.length >= 2 && !(x == "Backspace" || x == "Enter")) return;
+    if ( (/[a-zA-Z0-9 ]/).test(x) ) {
         e.preventDefault();
 
         if (e.key === "Enter") {

feaa8a53f708d2191b78fdf73e97b893ebeddf82
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-03-14 13:09:41 +0100

    dots

--- js/map-points.js
+++ js/map-points.js
@@ -4,8 +4,26 @@ const imgPaths = {
         // List of all dot coordinates placed around the map. Scaled to the image, then converted back into pixels.
         // Each dot needs a unique name
         dots: {
-            "dot1": {x: 0.48244, y: 0.07810},
-            
+            "dot1": {x: 0.17900, y: 0.38048}, // sala 2
+            "dot2": {x: 0.25200, y: 0.44272}, // sala 3
+            "dot3": {x: 0.25100, y: 0.74116}, // dyrektor
+            "dot4": {x: 0.24900, y: 0.88826}, // sala 10
+            "dot5": {x: 0.10200, y: 0.88119}, // sala 13
+            "dot6": {x: 0.10300, y: 0.65912}, // sekretariat uczniowski
+            "dot7": {x: 0.39300, y: 0.49646}, // sala 21
+            "dot8": {x: 0.53700, y: 0.49505}, // sala 24
+            "dot9": {x: 0.23700, y: 0.57709}, // maniek
+            "dot10": {x: 0.76000, y: 0.56011}, // sala 46
+            "dot11": {x: 0.83700, y: 0.63366}, // sala 45
+            "dot12": {x: 0.89100, y: 0.54173}, // kibel meski
+            "dot13": {x: 0.75900, y: 0.36775}, // psycholog
+            "dot14": {x: 0.76100, y: 0.21075}, // sala 32
+            "dot15": {x: 0.76100, y: 0.10750}, // sala bez numerku raz byla na niemieckim i historii
+            "dot16": {x: 0.91300, y: 0.16124}, // sala 40 znak zapytania
+            "dot17": {x: 0.91100, y: 0.29562}, // sala 41
+            "dot18": {x: 0.91000, y: 0.48091}, // schody b gora
+            "dot19": {x: 0.90900, y: 0.43706}, // schody b dol
+            "dot20": {x: 0.09200, y: 0.52900}, // schody a gora
         }
 
     },
@@ -59,6 +77,64 @@ function pxToPercent(pixels, relativeElement) {
     }
 }
 
+// for each img, resize the image-canvas-container to the img's dimensions
+const imgContainers = document.querySelectorAll(".image-canvas-container");
+imgContainers.forEach(container => {
+    let img = container.querySelector("img")
+    let canvas = container.querySelector("canvas");
+
+    container.style.width = `${img.width}px`;
+    container.style.height = `${img.height}px`;
+})
+
+
+// draw img paths
+
+for (const [floorName, floorItems] of Object.entries(imgPaths)) {
+
+    for (const [dot, coords] of Object.entries(floorItems.dots)) {
+        let floorContainer = document.querySelector(`div.${floorName}`);
+        let img = floorContainer.querySelector("img");
+        let canvas = floorContainer.querySelector("canvas");
+    
+
+        let draw = function() {
+            let coordsPx = percentToPx(coords, img);
+        
+            canvas.width = img.width;
+            canvas.height = img.height;
+        
+            let ctx = canvas.getContext("2d");
+            ctx.fillStyle = "red";
+            ctx.beginPath();
+            ctx.arc(coordsPx.x, coordsPx.y, 5, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+
+        if (img.complete) {
+            draw();
+        } else {
+            img.onload = draw;
+        }
+        
+    }
+
+}
+
+for (const [dot, coords] of Object.entries(imgPaths.Floor0.dots)) {
+    let floorContainer = document.querySelector("div.Floor0");
+    let img = floorContainer.querySelector("img");
+    let canvas = floorContainer.querySelector("canvas");
+
+    let coordsPx = percentToPx(coords, img);
+    
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(coordsPx.x, coordsPx.y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    
+}
 
 
 // const img = document.querySelector(".map img");

b69d20356b2b50df9f62583b9011197d8db46212
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-03-17 21:56:51 +0100

    Map connections & descriptions finished
    Czcionka do budynków:
    MS PGothic

--- js/map-points.js
+++ js/map-points.js
@@ -4,27 +4,27 @@ const imgPaths = {
         // List of all dot coordinates placed around the map. Scaled to the image, then converted back into pixels.
         // Each dot needs a unique name
         dots: {
-            "dot2": { x: 0.18500, y: 0.38048 }, // sala 2
-            "dot3": { x: 0.25200, y: 0.46108 }, // sala 3
-            "dot4": { x: 0.25100, y: 0.74882 }, // dyrektor
-            "dot5": { x: 0.24900, y: 0.87972 }, // sala 10
-            "dot6": { x: 0.10200, y: 0.87972 }, // sala 13
-            "dot7": { x: 0.10300, y: 0.65912 }, // sekretariat uczniowski
-            "dot8": { x: 0.39300, y: 0.49505 }, // sala 22
-            "dot9": { x: 0.53700, y: 0.49505 }, // sala 24
-            "dot10": { x: 0.76000, y: 0.56011 }, // sala 46
-            "dot11": { x: 0.83700, y: 0.63366 }, // sala 45
-            "dot12": { x: 0.89100, y: 0.54173 }, // kibel meski
-            "dot13": { x: 0.75900, y: 0.36775 }, // psycholog
-            "dot14": { x: 0.76100, y: 0.21075 }, // sala 32
-            "dot15": { x: 0.76100, y: 0.10750 }, // sala bez numerku raz byla na niemieckim i historii
-            "dot16": { x: 0.91300, y: 0.16124 }, // sala 40 znak zapytania
-            "dot17": { x: 0.91100, y: 0.29562 }, // sala 41
-            "dot18": { x: 0.91000, y: 0.47524 }, // schody b gora
-            "dot19": { x: 0.90900, y: 0.43706 }, // schody b dol
-            "dot20": { x: 0.09200, y: 0.52900 }, // schody a gora
-            "dot21": { x: 0.62083, y: 0.55896 }, // mirys SALA
-            "dot22": { x: 0.10667, y: 0.46226 }, // wc A
+            "dot2": { x: 0.18500, y: 0.38048, desc: "Sala 2 (girl team)" }, // sala 2
+            "dot3": { x: 0.25200, y: 0.46108, desc: "Sala 3 (historia)" }, // sala 3
+            "dot4": { x: 0.25100, y: 0.74882, desc: "Dyrektor" }, // dyrektor
+            "dot5": { x: 0.24900, y: 0.87972, desc: "Sala 10 (j. polski)" }, // sala 10
+            "dot6": { x: 0.10200, y: 0.87972, desc: "Sala 13 (matematyka)" }, // sala 13
+            "dot7": { x: 0.10300, y: 0.65912, desc: "Sekretariat uczniowski" }, // sekretariat uczniowski
+            "dot8": { x: 0.39300, y: 0.49505, desc: "Sala 22 (technik automatyk)" }, // sala 22
+            "dot9": { x: 0.53700, y: 0.49505, desc: "Sala 24 (sumoboty)" }, // sala 24
+            "dot10": { x: 0.76000, y: 0.56011, desc: "Sala 46 (filozofia, religia)" }, // sala 46
+            "dot11": { x: 0.83700, y: 0.63366, desc: "Sala 45 (j. angielski)" }, // sala 45
+            "dot12": { x: 0.89100, y: 0.54173, desc: "Toaleta A (1)" }, // kibel meski
+            "dot13": { x: 0.75900, y: 0.36775, desc: "Psycholog" }, // psycholog
+            "dot14": { x: 0.76100, y: 0.21075, desc: "Sala 32 (chemia, biologia)" }, // sala 32
+            "dot15": { x: 0.76100, y: 0.10750, desc: "Sala 39 (technik energetyk*)" }, // sala bez numerku raz byla na niemieckim i historii
+            "dot16": { x: 0.91300, y: 0.16124, desc: "Sala 40 (technik mechatronik)" }, // sala 40 znak zapytania
+            "dot17": { x: 0.91100, y: 0.29562, desc: "Sala 40 (technik mechatronik)" }, // sala 41
+            "dot18": { x: 0.91000, y: 0.47524, desc: "SCHODY B - GÓRA" }, // schody b gora
+            "dot19": { x: 0.90900, y: 0.43706, desc: "SCHODY B - DÓL" }, // schody b dol
+            "dot20": { x: 0.09200, y: 0.52900, desc: "SCHODY A - GÓRA" }, // schody a gora
+            "dot21": { x: 0.62083, y: 0.55896, desc: "Praktyka zawodowa (Ryszard Mirys)" }, // mirys SALA
+            "dot22": { x: 0.10667, y: 0.46226, desc: "Toaleta A (2)" }, // wc A
 
             // W dots: walk dots
             // You can not go back in this pathfinding system, so the connections are only one way
@@ -70,41 +70,48 @@ const imgPaths = {
         dots: {
             "dot1A": { x: 0.17978, y: 0.37726 }, // sala 102
             "dot2A": { x: 0.25200, y: 0.44169 }, // sala 103
-            "dot3A": { x: 0.10422, y: 0.46370 }, // Toaleta meska A
-            "dot4A": { x: 0.10200, y: 0.63186 }, // Toaleta pracownicza A
-            "dot5A": { x: 0.10200, y: 0.68686 }, // sala 119
-            "dot6A": { x: 0.41644, y: 0.49513 }, // Sala gimnastyczna
-            "dot7A": { x: 0.26867, y: 0.71279 }, // Pielegniarka A
+            "dot3A": { x: 0.10422, y: 0.46370, desc: "Toaleta A (1)" }, // Toaleta meska A
+            "dot4A": { x: 0.10200, y: 0.63186, desc: "Toaleta A (2)" }, // Toaleta pracownicza A
+            "dot5A": { x: 0.10200, y: 0.68686, desc: "Sala 119 (wolontariat)" }, // sala 119
+            "dot6A": { x: 0.41644, y: 0.49513, desc: "Sala gimnastyczna" }, // Sala gimnastyczna
+            "dot7A": { x: 0.26867, y: 0.71279, desc: "Pielęgniarka" }, // Pielegniarka A
 
-            "dot8B": { x: 0.75978, y: 0.53285 }, // Sala 123
-            "dot9B": { x: 0.83400, y: 0.61771 }, // Sala 122
-            "dot10B": { x: 0.90756, y: 0.37097 }, // Toaleta pracownicza B
-            "dot11B": { x: 0.90867, y: 0.25310 }, // Sala 138
-            "dot12B": { x: 0.90867, y: 0.14152 }, // Sala 134
-            "dot13B": { x: 0.76311, y: 0.14466 }, // Sala 132
-            "dot14B": { x: 0.76311, y: 0.26882 }, // Sala 131
+            "dot7.5A": { x: 0.25083, y: 0.84552, desc: "Sala 113 (TRN)" }, // Sala 113
+
+            "dot8B": { x: 0.75978, y: 0.53285, desc: "Sala 123 (technik programista)" }, // Sala 123
+            "dot9B": { x: 0.83400, y: 0.61771, desc: "Sala 122 (technik programista)" }, // Sala 122
+            "dot10B": { x: 0.90756, y: 0.37097, desc: "Toaleta B" }, // Toaleta pracownicza B
+            "dot11B": { x: 0.90867, y: 0.25310, desc: "Sala 138 (technik elektronik)" }, // Sala 138
+            "dot12B": { x: 0.90867, y: 0.14152, desc: "Sala 134 (technik elektryk)" }, // Sala 134
+            "dot13B": { x: 0.76311, y: 0.14466, desc: "Sala 135 (technik elektronik)" }, // Sala 135
+            "dot13.5B": { x: 0.75917, y: 0.43868 }, // Sala 131
+            "dot14B": { x: 0.76311, y: 0.26882 }, // Sala 132
 
             "Wdot1A": { x: 0.07700, y: 0.57426, connections: ['Wdot3A'] }, // schody dol A
             "Wdot2A": { x: 0.07500, y: 0.53041, connections: ['Wdot3A'] }, // schody gora A
-            "Wdot3A": { x: 0.18200, y: 0.53890, connections: ['Wdot11A', 'Wdot4A', 'Wdot6A'] }, // mid A
-            "Wdot4A": { x: 0.18100, y: 0.46393, connections: ['Wdot5A', 'dot3A'] }, // gora triple A
-            "Wdot5A": { x: 0.18100, y: 0.44272, connections: ['dot1A', 'dot2A'] }, // gora A przed prawa sala
-            "Wdot6A": { x: 0.18100, y: 0.63225, connections: ['dot4A', 'Wdot7A'] }, // przed WC pracownicze A
+            "Wdot3A": { x: 0.18000, y: 0.53890, connections: ['Wdot11A', 'Wdot4A', 'Wdot6A'] }, // mid A
+            "Wdot4A": { x: 0.18000, y: 0.46393, connections: ['Wdot5A', 'dot3A'] }, // gora triple A
+            "Wdot5A": { x: 0.18000, y: 0.44272, connections: ['dot1A', 'dot2A'] }, // gora A przed prawa sala
+            "Wdot6A": { x: 0.18000, y: 0.63225, connections: ['dot4A', 'Wdot7A'] }, // przed WC pracownicze A
             "Wdot7A": { x: 0.18000, y: 0.68600, connections: ['dot5A', 'Wdot8A'] }, // przed 119
-            "Wdot8A": { x: 0.18000, y: 0.73267, connections: ['Wdot9A'] }, // przed pielegniarka
+            "Wdot8A": { x: 0.18000, y: 0.73267, connections: ['Wdot9A', 'Wdot8.5A'] }, // przed pielegniarka
             "Wdot9A": { x: 0.22600, y: 0.73267, connections: ['Wdot10A'] }, // pielegniarka drzwi down
             "Wdot10A": { x: 0.22700, y: 0.71711, connections: ['dot7A'] }, // pielegniarka drzwi
             "Wdot11A": { x: 0.33400, y: 0.54031, connections: ['Wdot12A'] }, // szatnia sala gimnastyczna
             "Wdot12A": { x: 0.33400, y: 0.49505, connections: ['dot6A'] }, // szatnia przed sala gimnastyczna
 
+            "Wdot8.5A": { x: 0.18000, y: 0.84552, connections: ['dot7.5A'] }, // Sala 113 STAND
+
             "Wdot13B": { x: 0.92100, y: 0.43706, connections: ['Wdot15B'] }, // schody dol B
             "Wdot14B": { x: 0.92200, y: 0.47666, connections: ['Wdot15B'] }, // schody gora B
-            "Wdot15B": { x: 0.83300, y: 0.43706, connections: ['Wdot16B', 'Wdot17B'] }, // mid B
+            "Wdot15B": { x: 0.83400, y: 0.43706, connections: ['Wdot16B', 'Wdot17B', 'dot13.5B'] }, // mid B
             "Wdot16B": { x: 0.83400, y: 0.53285, connections: ['dot8B', 'dot9B'] }, // dol triple B
-            "Wdot17B": { x: 0.83300, y: 0.37341, connections: ['dot10B', 'Wdot18B'] }, // przed WC pracownicze B
-            "Wdot18B": { x: 0.83200, y: 0.26874, connections: ['dot14B', 'Wdot19B'] }, // przed 131
-            "Wdot19B": { x: 0.83300, y: 0.25318, connections: ['dot11B', 'Wdot20B'] }, // przed 138
-            "Wdot20B": { x: 0.83200, y: 0.14427, connections: ['dot12B', 'dot13B'] }, // przed 132 i 134
+            "Wdot17B": { x: 0.83400, y: 0.37341, connections: ['dot10B', 'Wdot18B'] }, // przed WC pracownicze B
+            "Wdot18B": { x: 0.83400, y: 0.26874, connections: ['dot14B', 'Wdot19B'] }, // przed 131
+            "Wdot19B": { x: 0.83400, y: 0.25318, connections: ['dot11B', 'Wdot20B'] }, // przed 138
+            "Wdot20B": { x: 0.83400, y: 0.14427, connections: ['dot12B', 'dot13B'] }, // przed 132 i 134
+
+
 
         }
 
@@ -112,13 +119,50 @@ const imgPaths = {
 
     "Floor2A": {
         dots: {
+            "dot1": { x: 0.53750, y: 0.21385, desc: "Sala 202 (j. angielski)" }, // Sala 202
+            "dot2": { x: 0.31250, y: 0.30308 }, // WC meskie
+            "dot3": { x: 0.31250, y: 0.51692 }, // WC zenskie
+            "dot4": { x: 0.74500, y: 0.28308 }, // Sala 203
+            "dot5": { x: 0.74500, y: 0.48923, desc: "Sala 204 (j. polski)" }, // Sala 204
+            "dot6": { x: 0.74500, y: 0.69077, desc: "Sala 205 (matematyka)" }, // Sala 205
+            "dot7": { x: 0.74500, y: 0.79846, desc: "Sala 207 (fizyka)" }, // Sala 207
+            "dot8": { x: 0.41500, y: 0.88308, desc: "Sala 210 (matematyka)" }, // Sala 210
+            "dot9": { x: 0.31250, y: 0.68923 }, // Biblioteka
 
+            "Wdot1": { x: 0.31000, y: 0.44000, connections: ['Wdot2'] }, // Schody w dol
+            "Wdot2": { x: 0.53500, y: 0.44000, connections: ['Wdot3', 'Wdot5'] }, // mid A
+            "Wdot3": { x: 0.53500, y: 0.30000, connections: ['Wdot4', 'dot2'] }, // przed WC up
+            "Wdot4": { x: 0.53500, y: 0.28308, connections: ['dot1', 'dot4'] }, // przed sale up
+            "Wdot5": { x: 0.53500, y: 0.49231, connections: ['dot5', 'Wdot6'] }, // przed 204
+            "Wdot6": { x: 0.53500, y: 0.51538, connections: ['dot3', 'Wdot7'] }, // przed WC down
+            "Wdot7": { x: 0.53500, y: 0.69231, connections: ['dot9', 'dot6', 'Wdot8'] }, // przed 205 i bibl
+            "Wdot8": { x: 0.53500, y: 0.79846, connections: ['dot7', 'Wdot9'] }, // przed 207
+            "Wdot9": { x: 0.41500, y: 0.79846, connections: ['dot8'] }, // przed 210
         }
 
     },
     "Floor2B": {
         dots: {
+            "dot1": { x: 0.50000, y: 0.89231, desc: "Sala 215 (j. niemiecki)" }, // Sala 215
+            "dot2": { x: 0.28750, y: 0.78769 }, // Sala 216
+            "dot3": { x: 0.28250, y: 0.62462 }, // Sala 219
+            "dot4": { x: 0.28250, y: 0.46154, desc: "Sala 221 (technik informatyk)" }, // Sala 221
+            "dot5": { x: 0.27500, y: 0.30615, desc: "Sala 222 (t. informatyk, programista)" }, // Sala 222
+            "dot6": { x: 0.29750, y: 0.23385, desc: "Sala 224 (technik informatyk)" }, // Sala 224
+            "dot7": { x: 0.70500, y: 0.23538, desc: "Sala 226 (technik informatyk)" }, // Sala 226
+            "dot8": { x: 0.69750, y: 0.37385 }, // Sala 227
+            "dot9": { x: 0.70500, y: 0.56154 }, // WC zenskie
+            "dot10": { x: 0.70500, y: 0.78769 }, // WC meskie
 
+            "Wdot1": { x: 0.71750, y: 0.65385, connections: ['Wdot2'] }, // Schody w dol
+            "Wdot2": { x: 0.50000, y: 0.65385, connections: ['Wdot4', 'Wdot3'] }, // mid B
+            "Wdot3": { x: 0.50000, y: 0.78769, connections: ['dot1', 'dot2', 'dot10'] }, // down B
+            "Wdot4": { x: 0.50000, y: 0.62308, connections: ['dot3', 'Wdot5'] }, // przed 219
+            "Wdot5": { x: 0.50000, y: 0.56462, connections: ['dot9', 'Wdot6'] }, // przed WC zenskie
+            "Wdot6": { x: 0.50000, y: 0.46308, connections: ['dot4', 'Wdot7'] }, // przed 221
+            "Wdot7": { x: 0.50000, y: 0.37385, connections: ['dot8', 'Wdot8'] }, // przed 227
+            "Wdot8": { x: 0.50000, y: 0.30923, connections: ['dot5', 'Wdot9'] }, // przed 222
+            "Wdot9": { x: 0.50000, y: 0.23538, connections: ['dot6', 'dot7'] }, // up B
         }
 
     },
@@ -189,7 +233,7 @@ function drawDotsEverywhere() {
 
 }
 
-// drawDotsEverywhere();
+drawDotsEverywhere();
 
 
 // Draw a line from one point to the other

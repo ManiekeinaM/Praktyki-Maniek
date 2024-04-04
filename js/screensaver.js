795181ea8d5cb452f1aa0d810da24ae1298915b8
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-04-04 12:15:55 +0200

    test2

--- js/screensaver.js
+++ js/screensaver.js
@@ -337,6 +337,7 @@ function initializeCamera() {
     video.addEventListener("play", function() {
         var draw = function() {
             if(video.paused || video.ended) return;
+            console.log('draw');
 
             ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
             let motion = motionDetection();

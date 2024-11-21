84ba82c601ea1798e05627fb71c873cb1b35274a
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2024-11-21 09:50:54 +0100

    mobilka, wisielec win/lose

--- js/games/wisielec.js
+++ js/games/wisielec.js
@@ -2,6 +2,8 @@ const keyboard = document.querySelector('.keyboard');
 const kbButtons = keyboard.querySelectorAll('.row button');
 const letters = document.querySelector('.letters');
 
+const result = document.querySelector('p.wordResult');
+const whatCategory = document.querySelector('p.category');
 
 const maniek_stages = Array.from(document.querySelectorAll('.maniek-stages > img'));
 function shuffleArray(array) {
@@ -11,6 +13,7 @@ function shuffleArray(array) {
     }
 }
 
+/*
 const WORDS = ["ogród", "drzewo", "krzew", "kwiat", "trawa", "liść", "korzeń", "gałąź", "owoc", "warzywo", "kapusta", "marchewka", "pietruszka", "cebula", "czosnek", "pomidor", "ogórek", "papryka", "ziemniak", "burak", "dynia", "fasola", "groszek", "sałata", "szpinak", "rzodkiewka", "brokuł", "kalafior", "brukselka", "por", "seler", "jarmuż", "szparagi", "rukola", "arbuzy", "winogrona", "gruszka", "jabłko", "śliwka", "morela", "brzoskwinia", "wiśnia", "czereśnia", "truskawka", "malina", "jagoda", "jeżyna", "porzeczka", "agrest", "figa", "kiwi", "ananas", 
     "mango", "banan", "cytryna", "limonka", "pomarańcza", "mandarynka", "grejpfrut", "granat", "kokos", "orzech", "migdał", "rodzynka", "żurawina", "daktyl", "imbir", "bazylia", "mięta", "tymianek", "pietruszka", "koper", "majeranek", "kolendra", "szałwia", "lubczyk", "melisa", "czarny", "biały", "czerwony", "zielony", "niebieski", "żółty", "pomarańczowy", "fioletowy", "różowy", "brązowy", "szary", "srebrny", "złoty", "beżowy", "turkusowy", "granatowy", "bordowy", "łazienka", "kuchnia", "salon", "sypialnia", "jadalnia", 
     "pokój", "garaż", "piwnica", "strych", "korytarz", "schody", "balkon", "taras", "ogród", "altana", "basen", "fontanna", "staw", "ławeczka", "huśtawka", "piaskownica", "trampolina", "grill", "kosz", "szafa", "komoda", "biurko", "krzesło", "fotel", "kanapa", "stolik", "lampa", "dywan", "zasłona", "firana", "obraz", "lustro", "półka", "regał", "telewizor", "radio", "komputer", "drukarka", "telefon", "tableta", "zegarek", "budzik", "kaloryfer", "klimatyzacja", "wentylator", "pralka", "zmywarka", "lodówka", "mikrofalówka", "piekarnik", "kuchenka", "toster", 
@@ -26,24 +29,113 @@ const WORDS = ["ogród", "drzewo", "krzew", "kwiat", "trawa", "liść", "korzeń
     "Adam", "Adrian", "Aleksander", "Andrzej", "Antoni", "Artur", "Bartłomiej", "Bartosz", "Błażej", "Dawid", "Dominik", "Filip", "Grzegorz", "Hubert", "Igor", "Jakub", "Jan", "Jerzy", "Kacper", "Kamil", "Karol", "Konrad", "Krzysztof", "Łukasz", "Maciej", "Marek", "Marcin", "Mariusz", "Mateusz", "Michał", "Patryk", "Paweł", "Piotr", "Radosław", "Rafał", "Robert", "Sebastian", "Sławomir", "Stanisław", "Szymon", "Tomasz", "Wiktor", "Wojciech", "Zbigniew", "Agnieszka", "Aleksandra", "Alicja", "Anna", "Barbara", "Beata", "Dominika", "Edyta", "Elżbieta", "Ewa",
     "Maniek", "super"
 ]
+*/
+
+const WORDS_SEGREGATED = {
+    imiona: [
+        "Adam", "Adrian", "Aleksander", "Andrzej", "Antoni", "Artur", "Bartłomiej",
+        "Bartosz", "Błażej", "Dawid", "Dominik", "Filip", "Grzegorz", "Hubert",
+        "Igor", "Jakub", "Jan", "Jerzy", "Kacper", "Kamil", "Karol", "Konrad",
+        "Krzysztof", "Łukasz", "Maciej", "Marek", "Marcin", "Mariusz", "Mateusz",
+        "Michał", "Patryk", "Paweł", "Piotr", "Radosław", "Rafał", "Robert",
+        "Sebastian", "Sławomir", "Stanisław", "Szymon", "Tomasz", "Wiktor",
+        "Wojciech", "Zbigniew", "Agnieszka", "Aleksandra", "Alicja", "Anna",
+        "Barbara", "Beata", "Dominika", "Edyta", "Elżbieta", "Ewa", "Maniek"
+    ],
+    zwierzęta: [
+        "nutria", "szop", "bóbr", "sarna", "jeleń", "łoś", "żubr", "wilk", "lis",
+        "niedźwiedź", "ryś", "dzik", "zając", "królik", "mysz", "wiewiórka", "kuna",
+        "jenot", "borsuk", "wydra", "orzeł", "sokół", "pustułka", "myszołów",
+        "czajka", "kaczka", "gęś", "łabędź", "bocian", "żaba", "ropucha", "traszka",
+        "jaszczurka", "wąż", "żmija", "zaskroniec", "krokodyl", "hipopotam", "słoń",
+        "żyrafa", "lew", "tygrys", "pantera", "gepard", "hiena", "antylopa",
+        "goryl", "szympans", "orangutan", "małpa", "lemur", "panda", "koala",
+        "kangur", "emu", "struś", "kiwi", "delfin", "wieloryb", "rekin",
+        "płaszczka", "meduza", "kałamarnica", "ośmiornica", "krab", "homar",
+        "krewetka", "małż", "ostryga", "ślimak", "gąbka", "koral"
+    ],
+    jedzenie: [
+        "owoc", "warzywo", "kapusta", "marchewka", "pietruszka", "cebula", "czosnek",
+        "pomidor", "ogórek", "papryka", "ziemniak", "burak", "dynia", "fasola",
+        "groszek", "sałata", "szpinak", "rzodkiewka", "brokuł", "kalafior",
+        "brukselka", "por", "seler", "jarmuż", "szparagi", "rukola", "arbuzy",
+        "winogrona", "gruszka", "jabłko", "śliwka", "morela", "brzoskwinia",
+        "wiśnia", "czereśnia", "truskawka", "malina", "jagoda", "jeżyna",
+        "porzeczka", "agrest", "figa", "kiwi", "ananas", "mango", "banan", "cytryna",
+        "limonka", "pomarańcza", "mandarynka", "grejpfrut", "granat", "kokos",
+        "orzech", "migdał", "rodzynka", "żurawina", "daktyl", "imbir", "bazylia",
+        "mięta", "tymianek", "pietruszka", "koper", "majeranek", "kolendra",
+        "szałwia", "lubczyk", "melisa"
+    ],
+    przedmiot: [
+        "grill", "kosz", "ławeczka", "huśtawka", "trampolina", "szafa", "komoda",
+        "biurko", "krzesło", "fotel", "kanapa", "stolik", "lampa", "dywan",
+        "zasłona", "firana", "obraz", "lustro", "półka", "regał", "telewizor",
+        "radio", "komputer", "drukarka", "telefon", "tablet", "zegarek", "budzik",
+        "kaloryfer", "klimatyzacja", "wentylator", "pralka", "zmywarka",
+        "lodówka", "mikrofalówka", "piekarnik", "kuchenka", "toster", "czajnik",
+        "ekspres", "odkurzacz", "żelazko", "suszarka", "kosiarka", "wiertarka",
+        "młotek", "śrubokręt", "piła", "kombinerki", "klucz", "lina", "taśma",
+        "klej", "farba", "pędzel", "wałek", "drabina", "wazon", "doniczka",
+        "nawóz", "grabie", "szpadel", "nożyce", "sekator", "wąż", "zraszacz",
+        "parasolka", "płaszcz", "kurtka", "sweter", "koszula", "bluzka", "spodnie",
+        "spódnica", "sukienka", "marynarka", "garnitur", "krawat", "pasek",
+        "skarpetki", "buty", "kapelusz", "czapka", "szalik", "rękawiczki",
+        "okulary", "torba", "plecak", "walizka", "portfel", "parasol", "rower",
+        "hulajnoga", "rolki", "łyżwy", "narty", "deskorolka", "kask",
+        "ochraniacze", "samochód", "motocykl", "autobus", "tramwaj", "pociąg",
+        "metro", "samolot", "helikopter", "statek", "łódź", "kajak", "żaglówka",
+        "balon", "rakieta", "bilet", "paszport", "dowód", "karta", "mapa",
+        "przewodnik", "namiot", "śpiwór", "karimata", "latarka", "kompas",
+        "termos", "aparat", "kamera", "lornetka", "notatnik", "długopis",
+        "ołówek", "gumka", "temperówka", "linijka", "nożyczki", "teleskop",
+        "mikroskop"
+    ],
+    miejsce: [
+        "łazienka", "kuchnia", "salon", "sypialnia", "jadalnia", "pokój", "garaż",
+        "piwnica", "strych", "korytarz", "schody", "balkon", "taras", "ogród",
+        "altana", "basen", "fontanna", "staw", "piaskownica", "hotel", "hostel",
+        "kemping", "jaskinia", "plaża", "pustynia", "ocean", "rzeka", "wodospad",
+        "wyspa", "wulkan", "góra", "szczyt", "dolina", "kanion", "las", "dżungla",
+        "sawanna", "półwysep", "zatoka", "cieśnina", "rafa", "polana",
+        "łąka", "skarpa", "klify", "pustkowie", "oaza", "lodowiec", "grodzisko",
+        "zamek", "pałac", "forteca", "katedra", "kościół", "synagoga", "meczet",
+        "świątynia", "ruiny", "pomnik", "muzeum", "galeria", "teatr", "opera",
+        "filharmonia", "sala", "amfiteatr", "stadion"
+    ],
+    inne: [
+        "czarny", "biały", "czerwony", "zielony", "niebieski", "żółty",
+        "pomarańczowy", "fioletowy", "różowy", "brązowy", "szary", "srebrny",
+        "złoty", "beżowy", "turkusowy", "granatowy", "bordowy", "ziemia",
+        "powietrze", "atmosfera", "chmura", "mgła", "rosa", "szron", "lód",
+        "śnieg", "deszcz", "grad", "burza", "błyskawica", "grzmot", "wiatr",
+        "tornado", "huragan", "pogoda", "klimat", "słońce", "księżyc", "gwiazda",
+        "planeta", "galaktyka", "wszechświat", "kosmos", "zwierzę", "ptak",
+        "ryba", "owad", "płaz", "gad", "ssak", "roślina", "liść", "kora",
+        "gałąź", "korzeń", "grunt", "gleba", "skała", "piasek", "glina", "ziemia", "super"
+    ]
+};
 
 // 657
 // console.log(WORDS.length);
 
 
-let WON = false;
 let GAME_STARTED = true;
 let CURRENT_WORD = '';
+let CURRENT_CATEGORY = '';
 let lettersToGuess = {};
 let FAILS = 0; // 6 fails = lose
 
 function getRandomWord() {
-    let word = WORDS[Math.floor(Math.random() * WORDS.length)];
-    return word.toLowerCase();
+    const categories = Object.keys(WORDS_SEGREGATED);
+    const category = categories[Math.floor(Math.random() * categories.length)];
+
+    const words = WORDS_SEGREGATED[category];
+    const word = words[Math.floor(Math.random() * words.length)];
+    return { category: category, word: word.toLowerCase() };
 }
 
 function makeGuess(letterButton) {
-   
     letterButton.classList.add('disabled');
     const letter = letterButton.textContent.toLowerCase();
     console.log(FAILS, letter);
@@ -72,10 +164,9 @@ function makeGuess(letterButton) {
 
     // Wygrana!
     // TODO popup na wygraną
-    WON = true;
     GAME_STARTED = false;
     initConfetti();
-    console.log("WYGRANA");
+    setTimeout(startGame, 5000);
 }
 
 function explodeFace() {
@@ -111,7 +202,7 @@ function explodeFace() {
 
 kbButtons.forEach(button => {
     button.addEventListener('click', e => {
-        if (!GAME_STARTED || WON) return;
+        if (!GAME_STARTED) return;
         if (e.target.classList.contains('disabled')) return;
 
         makeGuess(e.target);
@@ -122,11 +213,14 @@ kbButtons.forEach(button => {
 const explosion = document.querySelector('.explosion');
 function lose() {
     explosion.classList.add('explode');
+
+    result.innerText = `Słowem było: ${CURRENT_WORD}`;
+    setTimeout(startGame, 5000);
 }
 
 function reset() {
     FAILS = 0;
-    WON = false;
+    GAME_STARTED = true;
 
     // Clear keyboard
     kbButtons.forEach((button) => {
@@ -141,6 +235,12 @@ function reset() {
         face.style.display='';
     });
 
+    // Clear explosion
+    explosion.classList.remove('explode');
+    result.innerText = '';
+
+    lettersToGuess = {};
+
     // Randomize order of face
     shuffleArray(maniek_stages);
 }
@@ -153,7 +253,13 @@ function setupGuessBoard() {
         box.classList.add('letter');
         box.dataset.id = i;
         letters.appendChild(box);
+        if (i==0 && CURRENT_CATEGORY === 'imiona') {
+            box.style.textTransform = 'capitalize';
+        }
     }
+
+
+    whatCategory.innerText = `Kategoria: ${CURRENT_CATEGORY}`;
 }
 
 // Initialize playing the game
@@ -161,11 +267,15 @@ function startGame() {
     reset();
 
     GAME_STARTED = true;
-    CURRENT_WORD = getRandomWord();
+    let { category, word } = getRandomWord();
+    CURRENT_WORD = word;
+    CURRENT_CATEGORY = category;
+
     for (const letter of CURRENT_WORD) {
         lettersToGuess[letter] = true;
     }
     console.log(CURRENT_WORD);
+
 
     setupGuessBoard();
 }
